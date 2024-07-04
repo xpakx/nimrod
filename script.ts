9b1f3249a728a0f49348fe6f9a5b006e3ac08293
xpakx <55586470+xpakx@users.noreply.github.com>
2024-07-04 21:31:30 +0200

    feat: draw buildings on the plane

--- script.ts
+++ script.ts
@@ -41,7 +41,27 @@ function drawTile(ctx: CanvasRenderingContext2D, x: number, y: number, color: st
     ctx.restore();
 }
 
-window.onload = () => {
+async function loadImage(url: string): Promise<any> {
+    const image = new Image();
+    image.src = url;
+    return new Promise((resolve, reject) => {
+        image.onload = () => resolve(image);
+        image.onerror = reject;
+    });
+}
+
+function getSize(img: HTMLImageElement, widthNorm: number): number[] {
+	const width = tileWidth*widthNorm;
+	const height = img.height*(width/img.width);
+	return [width, height];
+}
+
+function putImage(ctx: CanvasRenderingContext2D, img: HTMLImageElement, position: number[], size: number[]) {
+	let pos = isoToScreen(position[0], position[1]);
+	ctx.drawImage(img, pos.x-size[0]/2, pos.y-size[1]+tileHeight, size[0], size[1]);
+}
+
+window.onload = async () => {
 	const canvas = document.getElementById('gameCanvas') as (HTMLCanvasElement | null);
 	if (!canvas) {
 		console.log("No canvas elem");
@@ -55,4 +75,23 @@ window.onload = () => {
 	canvas.width = canvasWidth;
 	canvas.height = canvasHeight;
 	drawIsometricMap(context);
+
+	const zigguratPos = [2, 2];
+	const ziggurat = await loadImage("./img/ziggurat.svg");
+	const zigguratSize = getSize(ziggurat, 3);
+
+	const homePos = [3, 3];
+	const home = await loadImage("./img/house.svg");
+	const homeSize = getSize(home, 1);
+
+	const towerPos = [4, 0];
+	const tower = await loadImage("./img/tower.svg");
+	const towerSize = getSize(tower, 1);
+
+        context.save();
+        context.translate(canvasWidth / 2, canvasHeight / 2 - (tileHeight/2)*5);
+	putImage(context, ziggurat, zigguratPos, zigguratSize);
+	putImage(context, home, homePos, homeSize);
+	putImage(context, tower, towerPos, towerSize);
+	context.restore();
 }
