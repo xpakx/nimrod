7902b913872ec3cb4a0ce41b42497573b6520b36
xpakx <55586470+xpakx@users.noreply.github.com>
2024-07-05 13:21:43 +0200

    refactor: create interfaces for size and position

--- script.ts
+++ script.ts
@@ -1,5 +1,5 @@
-const tileWidth = 64;
-const tileHeight = 32;
+const tileWidth = 128;
+const tileHeight = 64;
 const canvasWidth = 800;
 const canvasHeight = 600;
 
@@ -11,9 +11,19 @@ const map = [
     ['#FF5733', '#FFBD33', '#75FF33', '#33FF57', '#33FFBD']
 ];
 
-function isoToScreen(x: number, y: number) {
-    const screenX = (x - y) * (tileWidth / 2);
-    const screenY = (x + y) * (tileHeight / 2);
+interface Size {
+	width: number,
+	height: number,
+}
+
+interface Position {
+	x: number,
+	y: number,
+}
+
+function isoToScreen(pos: Position): Position {
+    const screenX = (pos.x - pos.y) * (tileWidth / 2);
+    const screenY = (pos.x + pos.y) * (tileHeight / 2);
     return { x: screenX, y: screenY };
 }
 
@@ -21,7 +31,7 @@ function drawIsometricMap(ctx: CanvasRenderingContext2D) {
     for (let y = 0; y < map.length; y++) {
         for (let x = 0; x < map[y].length; x++) {
             const color = map[y][x];
-            const screenPos = isoToScreen(x, y);
+            const screenPos = isoToScreen({x: x, y: y});
             drawTile(ctx, screenPos.x, screenPos.y, color);
         }
     }
@@ -50,15 +60,15 @@ async function loadImage(url: string): Promise<any> {
     });
 }
 
-function getSize(img: HTMLImageElement, widthNorm: number): number[] {
+function getSize(img: HTMLImageElement, widthNorm: number): Size {
 	const width = tileWidth*widthNorm;
 	const height = img.height*(width/img.width);
-	return [width, height];
+	return { width: width, height: height };
 }
 
-function putImage(ctx: CanvasRenderingContext2D, img: HTMLImageElement, position: number[], size: number[]) {
-	let pos = isoToScreen(position[0], position[1]);
-	ctx.drawImage(img, pos.x-size[0]/2, pos.y-size[1]+tileHeight, size[0], size[1]);
+function putImage(ctx: CanvasRenderingContext2D, img: HTMLImageElement, position: Position, size: Size) {
+	let pos = isoToScreen(position);
+	ctx.drawImage(img, pos.x-size.width/2, pos.y-size.height+tileHeight, size.width, size.height);
 }
 
 window.onload = async () => {
@@ -76,15 +86,16 @@ window.onload = async () => {
 	canvas.height = canvasHeight;
 	drawIsometricMap(context);
 
-	const zigguratPos = [2, 2];
+	const zigguratPos = {x: 1, y: 1};
 	const ziggurat = await loadImage("./img/ziggurat.svg");
-	const zigguratSize = getSize(ziggurat, 3);
+	const zigguratSize = getSize(ziggurat, 2);
 
-	const homePos = [3, 3];
+	const homePos = {x: 3, y: 3};
+	const home2Pos = {x: 4, y: 4};
 	const home = await loadImage("./img/house.svg");
 	const homeSize = getSize(home, 1);
 
-	const towerPos = [4, 0];
+	const towerPos = {x: 4, y: 0};
 	const tower = await loadImage("./img/tower.svg");
 	const towerSize = getSize(tower, 1);
 
@@ -92,6 +103,7 @@ window.onload = async () => {
         context.translate(canvasWidth / 2, canvasHeight / 2 - (tileHeight/2)*5);
 	putImage(context, ziggurat, zigguratPos, zigguratSize);
 	putImage(context, home, homePos, homeSize);
+	putImage(context, home, home2Pos, homeSize);
 	putImage(context, tower, towerPos, towerSize);
 	context.restore();
 }
