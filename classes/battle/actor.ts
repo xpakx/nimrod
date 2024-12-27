cb9c4358db53c02b5b0480aa1b1135a1f6f76f36
xpakx <55586470+xpakx@users.noreply.github.com>
2024-12-27 16:12:46 +0100

    feat: let battle actors move

--- classes/battle/actor.ts
+++ classes/battle/actor.ts
@@ -1,5 +1,6 @@
 import { Actor } from "../actor.js";
 import { Road } from "../buildings.js";
+import { Position } from "../map-layer.js";
 
 export class BattleActor extends Actor {
 	enemy: boolean = false;
@@ -10,7 +11,45 @@ export class BattleActor extends Actor {
 	hp: number = 0;
 	skills: Skill[] = []
 
-	tick(_deltaTime: number, _roads: (Road | undefined)[][], _randMap: number[]): boolean {
+	moving: boolean = false;
+	path?: Position[]
+	goal?: Position;
+	lastPosition: Position = {x: 0, y: 0};
+
+	tick(deltaTime: number, _roads: (Road | undefined)[][], _randMap: number[]): boolean {
+		return this.move(deltaTime);
+	}
+
+	reachedGoal(): boolean {
+		if (!this.goal) {
+			return true;
+		}
+		return this.positionSquare.x == this.goal.x && this.positionSquare.y == this.goal.y;
+	}
+
+	move(deltaTime: number): boolean {
+		if(!this.goal) {
+			return false;
+		}
+		console.log("goal:",this.goal);
+		console.log("curr:",this.positionSquare);
+		console.log("dir:", this.direction);
+		if (this.reachedGoal()) {
+			console.log("new goal");
+			this.goal = this.path?.pop();
+			console.log(this.goal);
+			if (this.goal) {
+				this.direction.x = this.goal.x - this.lastPosition.x;
+				this.direction.y = this.goal.y - this.lastPosition.y;
+				this.lastPosition.x = this.positionSquare.x;
+				this.lastPosition.y = this.positionSquare.y;
+			}
+		}
+
+		this.position.x = this.position.x + this.direction.x*deltaTime;
+		this.position.y = this.position.y + this.direction.y*deltaTime;
+		this.positionSquare.x = Math.floor(this.position.x);
+		this.positionSquare.y = Math.floor(this.position.y);
 		return false;
 	}
 }
