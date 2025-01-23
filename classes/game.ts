56e8be6be039e48e939f0b55497abb207bae6f8d
xpakx <55586470+xpakx@users.noreply.github.com>
2025-01-23 17:05:19 +0100

    refactor: split state calculation method

--- classes/game.ts
+++ classes/game.ts
@@ -526,11 +526,22 @@ export class Game {
 	}
 
 	calcState(deltaTime: number) {
+		const minuteEnded = this.advanceMinuteCounter(deltaTime);
+		this.calcBuildingsState(deltaTime, minuteEnded);
+		this.calcPedestriansState(deltaTime);
+		if(minuteEnded) this.spawnMigrants();
+	}
+
+	advanceMinuteCounter(deltaTime: number): boolean {
 		this.minuteCounter += deltaTime;
-		let minuteEnded = this.minuteCounter >= 60;
-		if(minuteEnded) {
+		if(this.minuteCounter >= 60) {
 			this.minuteCounter = 0;
+			return true;
 		}
+		return false;
+	}
+
+	calcBuildingsState(deltaTime: number, minuteEnded: boolean) {
 		for(let building of this.map.buildings) {
 			building.tick(deltaTime);
 			if(building.canSpawnWorker()) {
@@ -539,13 +550,16 @@ export class Game {
 			}
 			if(minuteEnded) building.onMinuteEnd(this.state);
 		}
-		const dTime = deltaTime > 0.5 ? 0.5 : deltaTime;
+	}
 
+	calcPedestriansState(deltaTime: number) {
+		const dTime = deltaTime > 0.5 ? 0.5 : deltaTime;
 		let randMap = [
 			Math.floor(Math.random() * 2),
 			Math.floor(Math.random() * 3),
 			Math.floor(Math.random() * 4),
-		]
+		];
+
 		let pedestrians = this.state.pedestrians;
 		this.state.pedestrians = [];
 		for(let pedestrian of pedestrians) {
@@ -556,29 +570,30 @@ export class Game {
 				this.state.population += 1;
 			}
 		}
-		if(minuteEnded) {
-			this.logger.debug("Spawning migrants");
-			// TODO
-			const freePlaces = this.state.maxPopulation - this.state.population;
-			const newMigrants = Math.min(20, freePlaces);
-			const emptyHouses = this.map.buildings
-				.filter(x => x instanceof House)
-				.filter(x => x.population < x.maxPopulation)
-			if (emptyHouses.length > 0) {
-				for (let i=0; i<newMigrants; i++) {
-					const migrant = new Migrant(this.sprites.actors["test"], {x: 0, y: 0});
-					const randomIndex = Math.floor(Math.random() * emptyHouses.length);
-					const emptyHouse = emptyHouses[randomIndex];
-					const path = this.map.shortestMigrantPath(migrant.positionSquare, emptyHouse);
-					if (path.length > 0) {
-						this.logger.debug("Path for migrant:", path);
-						migrant.setHome(emptyHouse, path);
-						this.state.insertPedestrian(migrant);
-					}
-				}
+	}
+	
+	spawnMigrants() {
+		this.logger.debug("Spawning migrants");
+		// TODO
+		const freePlaces = this.state.maxPopulation - this.state.population;
+		const newMigrants = Math.min(20, freePlaces);
+		const emptyHouses = this.getEmptyHouses();
+		if (emptyHouses.length == 0) return;
+		for (let i=0; i<newMigrants; i++) {
+			const migrant = new Migrant(this.sprites.actors["test"], {x: 0, y: 0});
+			const randomIndex = Math.floor(Math.random() * emptyHouses.length);
+			const emptyHouse = emptyHouses[randomIndex];
+			const path = this.map.shortestMigrantPath(migrant.positionSquare, emptyHouse);
+			if (path.length > 0) {
+				this.logger.debug("Path for migrant:", path);
+				migrant.setHome(emptyHouse, path);
+				this.state.insertPedestrian(migrant);
 			}
 		}
+	}
 
+	getEmptyHouses(): House[] {
+		return this.map.buildings.filter(x => x instanceof House).filter(x => x.population < x.maxPopulation);
 	}
 
 	renderGame(context: CanvasRenderingContext2D, deltaTime: number) {
