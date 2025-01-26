36b50e730f4caf56581b2178c8e344b9fd7254dd
xpakx <55586470+xpakx@users.noreply.github.com>
2025-01-26 21:45:48 +0100

    feat: add storage

---
+++ classes/building/storage.ts
@@ -0,0 +1,30 @@
+import { Building, BuildingPrototype } from "../buildings.js";
+import { MapLayer, Position } from "../map-layer.js";
+
+export class Storage extends Building {
+	storage: { [key: string]: number } = { "water": 0, "food": 0 }; // TODO
+	deliveryInProgress: boolean = false;
+
+	constructor(prototype: BuildingPrototype, position: Position, accepted: boolean = true) {
+		super(prototype, position, accepted);
+		this.initializeResources();
+	}
+
+	initializeResources() {
+
+	}
+	
+	order(resource: string, amount: number, building: Building, map: MapLayer): number {
+		if (!this.canSpawnWorker()) return 0;
+		if (!this.workerSpawn) return 0;
+		if (!building.workerSpawn) return 0;
+		if (!map.getNextStep(this.workerSpawn, building.workerSpawn)) return 0;
+		
+		const inStorage = resource in this.storage ? this.storage[resource] : 0;
+		const toDeliver = Math.min(inStorage, amount);
+		this.storage[resource] -= toDeliver;
+
+		// TODO: send worker;
+		return toDeliver;
+	}
+}
