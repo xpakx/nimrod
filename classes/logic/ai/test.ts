371cd003c37e6738f37ac942cf0a48de60cda3a4
xpakx <55586470+xpakx@users.noreply.github.com>
2025-06-16 17:31:48 +0200

    feat: add basic random battle AI for testing purposes

--- classes/logic/ai/test.ts
+++ classes/logic/ai/test.ts
@@ -6,5 +6,44 @@ import { MoveGenerator } from "./move.js";
 export class TestMoveGenerator implements MoveGenerator {
 
     makeMove(game: Game, actors: BattleActor[]): void {
+	    for (let actor of actors) {
+		    this.makeRandomMove(game, actor);
+	    }
     }
+
+    getRandomDirs(): Direction[] {
+	    const directions = [
+		    { dx: 0, dy: -1 },
+		    { dx: 0, dy: 1 },
+		    { dx: -1, dy: 0 },
+		    { dx: 1, dy: 0 }
+	    ];
+
+	    return [...directions].sort(() => Math.random() - 0.5);
+    }
+
+    makeRandomMove(game: Game, actor: BattleActor) {
+	    const dirPriority = this.getRandomDirs();
+
+	    for (const dir of dirPriority) {
+		    const newX = actor.positionSquare.x + dir.dx;
+		    const newY = actor.positionSquare.y + dir.dy;
+		    const position = {x: newX, y: newY};
+		    
+		    if (game.map.onMap(position)) {
+			    this.moveActor(game, actor, position);
+			    return;
+		    }
+	    }
+        
+    }
+
+    moveActor(game: Game, actor: BattleActor, position: Position) {
+	    game.battleLogic.moveActor(actor, position, [actor.positionSquare, position]);
+    }
+}
+
+interface Direction {
+	dx: number;
+	dy: number;
 }
