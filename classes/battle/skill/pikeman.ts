752d187269ea99566b1f63743b7b60c601e125c4
xpakx <55586470+xpakx@users.noreply.github.com>
2025-07-11 17:18:31 +0200

    refactor: extract checking allied unit type to new function

--- classes/battle/skill/pikeman.ts
+++ classes/battle/skill/pikeman.ts
@@ -1,22 +1,23 @@
+import { BattleActor } from "../actor.js";
 import { SkillEffectDamage, SkillEffectPassive } from "./skill.js";
 
 export let pikemanPassive: SkillEffectPassive = {
     type: "passive",
     handler: (passiveOwner, event, _actors, map) => {
-	    if (passiveOwner.enemy != event.source.enemy) return;
-	    if (passiveOwner.name != event.source.name) return;
-	    if (passiveOwner === event.source) return;
+	    if (!isSameAllyType(passiveOwner, event.source)) return;
 	    const dist = map.getTaxicabDistance(passiveOwner.positionSquare, event.source.positionSquare);
 	    if (dist == 1) event.criticalHit = true;
     },
     shouldApply: (passiveOwner, actor, _actors, map) => {
-	    if (passiveOwner.enemy != actor.enemy) return false;
-	    if (passiveOwner.name != actor.name) return false;
-	    if (passiveOwner === actor) return false;
+	    if (!isSameAllyType(passiveOwner, actor)) return false;
 	    const dist = map.getTaxicabDistance(passiveOwner.positionSquare, actor.positionSquare);
 	    return dist == 1;
     },
 
+}
+
+function isSameAllyType(a: BattleActor, b: BattleActor) {
+    return a.enemy == b.enemy && a.name == b.name && a !== b;
 }
 
 export let pikemanSkill001: SkillEffectDamage = {
