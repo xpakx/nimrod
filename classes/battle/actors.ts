9ebecbe7c3399942249bf05d3cd2d50f97db867c
xpakx <55586470+xpakx@users.noreply.github.com>
2025-07-12 21:36:12 +0200

    feat: add basic comparision methods to Heroes utils class

--- classes/battle/actors.ts
+++ classes/battle/actors.ts
@@ -8,4 +8,20 @@ export class Heroes {
     static isSameAllyType(a: BattleActor, b: BattleActor) {
         return a.enemy === b.enemy && a.name === b.name && a !== b;
     }
+
+    static areEnemies(a: BattleActor, b: BattleActor) {
+        return a.enemy !== b.enemy;
+    }
+
+    static areAllies(a: BattleActor, b: BattleActor) {
+        return a.enemy === b.enemy;
+    }
+
+    static getAlliesOf(actor: BattleActor, actors: BattleActor[]) {
+        return actors.filter(a => this.areAllies(actor, a));
+    }
+
+    static getEnemiesOf(actor: BattleActor, actors: BattleActor[]) {
+        return actors.filter(a => this.areEnemies(actor, a));
+    }
 }
