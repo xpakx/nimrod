628b5d6a7047eb7ce0217f4e934a666800f6b68d
xpakx <55586470+xpakx@users.noreply.github.com>
2025-07-18 22:56:32 +0200

    feat: create helper methods for creating passives

--- classes/battle/skills.ts
+++ classes/battle/skills.ts
@@ -20,5 +20,61 @@ export class Skills {
 	): SkillEffectPassive<"onSkill"> {
 		return { type: "passive", hook: "onSkill", handler, shouldApply };
 	}
+
+	static onKillPassive(
+		handler: HookHandlerMap["onKill"],
+		shouldApply?: Applychecker
+	): SkillEffectPassive<"onKill"> {
+		return { type: "passive", hook: "onKill", handler, shouldApply };
+	}
+
+	static onMovePassive(
+		handler: HookHandlerMap["onMove"],
+		shouldApply?: Applychecker
+	): SkillEffectPassive<"onMove"> {
+		return { type: "passive", hook: "onMove", handler, shouldApply };
+	}
+
+	static postSkillPassive(
+		handler: HookHandlerMap["postSkill"],
+		shouldApply?: Applychecker
+	): SkillEffectPassive<"postSkill"> {
+		return { type: "passive", hook: "postSkill", handler, shouldApply };
+	}
+
+	static onStatusAppliedPassive(
+		handler: HookHandlerMap["onStatusApplied"],
+		shouldApply?: Applychecker
+	): SkillEffectPassive<"onStatusApplied"> {
+		return { type: "passive", hook: "onStatusApplied", handler, shouldApply };
+	}
+
+	static preDamagePassive(
+		handler: HookHandlerMap["preDamage"],
+		shouldApply?: Applychecker
+	): SkillEffectPassive<"preDamage"> {
+		return { type: "passive", hook: "preDamage", handler, shouldApply };
+	}
+
+	static onDamagePassive(
+		handler: HookHandlerMap["onDamage"],
+		shouldApply?: Applychecker
+	): SkillEffectPassive<"onDamage"> {
+		return { type: "passive", hook: "onDamage", handler, shouldApply };
+	}
+
+	static onTurnStartPassive(
+		handler: HookHandlerMap["onTurnStart"],
+		shouldApply?: Applychecker
+	): SkillEffectPassive<"onTurnStart"> {
+		return { type: "passive", hook: "onTurnStart", handler, shouldApply };
+	}
+
+	static onTurnEndPassive(
+		handler: HookHandlerMap["onTurnEnd"],
+		shouldApply?: Applychecker
+	): SkillEffectPassive<"onTurnEnd"> {
+		return { type: "passive", hook: "onTurnEnd", handler, shouldApply };
+	}
 }
 
