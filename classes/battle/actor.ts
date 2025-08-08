d3098d1b56f7e98c0910caa93bcd228b366243ed
xpakx <55586470+xpakx@users.noreply.github.com>
2025-08-08 19:05:32 +0200

    feat: add tokens/buffs/debuffs to heroes

--- classes/battle/actor.ts
+++ classes/battle/actor.ts
@@ -19,6 +19,18 @@ export interface HeroStats {
 	vampirism: number;
 }
 
+interface Token {
+	name: string;
+	value?: number;
+	duration?: number;
+}
+
+interface Modifier {
+	stat: keyof HeroStats;
+	value: number;
+	duration?: number;
+}
+
 export class BattleActor extends Actor {
 	enemy: boolean = false;
 	placed: boolean = false;
@@ -46,6 +58,12 @@ export class BattleActor extends Actor {
 	private stats: HeroStats;
 	private modifiers: HeroStats;
 	private equipmentMods: HeroStats;
+
+	private tokens: Record<string, Token[]> = {};
+	private buffs: Record<string, Modifier[]> = {};
+	private debuffs: Record<string, Modifier[]> = {};
+
+	// TODO: tokens and temporary mods
 
 	constructor(sprite: ActorSprite, position: Position) {
 		super(sprite, position);
@@ -167,6 +185,99 @@ export class BattleActor extends Actor {
 			return true;
 		}
 		return false;
+	}
+
+	hasToken(key: string): boolean {
+		if(!(key in this.tokens)) return false;
+		return this.tokens[key].length > 0;
+	}
+
+	addToken(key: string, value: number = 1, duration: number | undefined = undefined) {
+		if(!(key in this.tokens)) this.tokens[key] = [];
+		this.tokens[key].push({name: key, value: value, duration: duration});
+	}
+
+	totalTokens(key: string): number {
+		if(!(key in this.tokens)) return 0;
+		return this.tokens[key].length;
+	}
+
+	resetTokens(key: string) {
+		this.tokens[key] = [];
+	}
+
+
+	isBuffed(key: keyof HeroStats): boolean {
+		if(!(key in this.buffs)) return false;
+		return this.buffs[key].length > 0;
+	}
+
+	addBuff(key: keyof HeroStats, value: number, duration: number | undefined = undefined) {
+		if(!(key in this.buffs)) this.buffs[key] = [];
+		this.buffs[key].push({stat: key, value: value, duration: duration});
+		this.modifiers[key] += value;
+	}
+
+
+	isDebuffed(key: keyof HeroStats): boolean {
+		if(!(key in this.debuffs)) return false;
+		return this.debuffs[key].length > 0;
+	}
+
+	addDebuff(key: keyof HeroStats, value: number, duration: number | undefined = undefined) {
+		if(!(key in this.debuffs)) this.debuffs[key] = [];
+		this.debuffs[key].push({stat: key, value: value, duration: duration});
+		this.modifiers[key] -= value;
+	}
+
+	advanceTokens() {
+		for (let key in this.tokens) {
+			let newTokens = [];
+			for (let value of this.tokens[key]) {
+				if (value.duration !== undefined) {
+					value.duration -= 1;
+					if (value.duration < 0) continue;
+				}
+				newTokens.push(value);
+			}
+			this.tokens[key] = newTokens;
+		}
+	}
+
+	advanceBuffs() {
+		for (let key in this.buffs) {
+			let newBuffs = [];
+			for (let value of this.buffs[key]) {
+				if (value.duration !== undefined) {
+					value.duration -= 1;
+					if (value.duration < 0) {
+						const k = key as keyof HeroStats; // TODO: type safety
+						this.modifiers[k] -= value.value;
+						continue;
+					}
+				}
+				newBuffs.push(value);
+			}
+			this.buffs[key] = newBuffs;
+		}
+	}
+
+	advanceDebuffs() {
+		for (let key in this.debuffs) {
+			let newDebuffs = [];
+			for (let value of this.debuffs[key]) {
+				if (value.duration !== undefined) {
+					value.duration -= 1;
+					if (value.duration < 0) {
+						const k = key as keyof HeroStats; // TODO: type safety
+						this.modifiers[k] += value.value;
+						continue;
+					}
+				}
+				newDebuffs.push(value);
+			}
+			this.debuffs[key] = newDebuffs;
+		}
 	}
 }
 
