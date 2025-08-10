d88771b4361009feee8584bce9ba7f9c0e527267
xpakx <55586470+xpakx@users.noreply.github.com>
2025-08-10 17:24:24 +0200

    feat: add heal/buff/token skill effects

--- classes/battle/skill/skill.ts
+++ classes/battle/skill/skill.ts
@@ -1,5 +1,5 @@
 import { MapLayer } from "../../map-layer.js";
-import { BattleActor, HeroType } from "../actor.js";
+import { BattleActor, HeroStats, HeroType } from "../actor.js";
 import { EffectHook, HookHandlerMap } from "../effect-system";
 
 export interface Skill {
@@ -15,7 +15,8 @@ export interface Skill {
 	passive: boolean;
 }
 
-export type SkillEffect = SkillEffectDamage | SkillEffectPassive;
+export type SkillEffect = SkillEffectDamage | SkillEffectPassive | SkillEffectBuff |
+	SkillEffectToken | SkillEffectHeal;
 
 export type DamageFunction = (hero: BattleActor, target: BattleActor, skill: Skill) => number; 
 
@@ -41,6 +42,7 @@ export interface SkillEffectPassive<T extends EffectHook = EffectHook> {
 	hook: T;
 	handler: HookHandlerMap[T],
 	shouldApply?: Applychecker;
+	specialEffects?: SpecialEffect[];
 }
 
 export interface SpecialEffect<T extends EffectHook = EffectHook> {
@@ -48,4 +50,45 @@ export interface SpecialEffect<T extends EffectHook = EffectHook> {
 	hook: T;
 	handler: HookHandlerMap[T],
 	descriptors: string[];
+}
+
+export type BuffFunction = (hero: BattleActor, actors: BattleActor[], map: MapLayer) => number; 
+
+export interface SkillEffectBuff {
+	type: "buff";
+	buffType: "buff" | "debuff";
+	stat: keyof HeroStats;
+	duration: number,
+	value?: number;
+	percentage?: number;
+	chance?: number; // undefined -> 100%
+	onExpire?: BuffFunction;
+	onCreation?: BuffFunction;
+	onTick?: BuffFunction;
+	specialEffects?: SpecialEffect[];
+}
+
+export type TokenFunction = (hero: BattleActor, token: string, actors: BattleActor[], map: MapLayer) => number; 
+
+export interface SkillEffectToken {
+	type: "token";
+	tokenName: string;
+	duration: number;
+	value?: number;
+	chance?: number; // undefined -> 100%
+	onExpire?: TokenFunction;
+	onCreation?: TokenFunction;
+	onTick?: TokenFunction;
+	specialEffects?: SpecialEffect[];
+}
+
+export type HealFunction = (hero: BattleActor, actors: BattleActor[], map: MapLayer) => number; 
+
+export interface SkillEffectHeal {
+	type: "heal";
+	tags: string[];
+	value?: number;
+	percentage?: number;
+	chance?: number; // undefined -> 100%
+	specialEffects?: SpecialEffect[];
 }
